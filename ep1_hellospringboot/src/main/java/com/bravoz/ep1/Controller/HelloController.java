4e2fbcc70101d9c41887936fea4dd3bfee63bcce
Bartolomeo <1050602964@qq.com>
2019-09-02 11:27:16 +0800

    Signed-off-by: Bartolomeo <1050602964@qq.com>

---
+++ ep1_hellospringboot/src/main/java/com/bravoz/ep1/Controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.bravoz.ep1.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/index")
+    public String index(){
+        return "Hello Spring Boot...";
+    }
+}
