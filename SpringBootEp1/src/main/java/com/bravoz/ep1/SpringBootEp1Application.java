45c9443b1a658c91e49e27e2f443498f3917b93d
Bartolomeo <1050602964@qq.com>
2019-09-02 11:04:02 +0800

    Signed-off-by: Bartolomeo <1050602964@qq.com>

---
+++ SpringBootEp1/src/main/java/com/bravoz/ep1/SpringBootEp1Application.java
@@ -0,0 +1,13 @@
+package com.bravoz.ep1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEp1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootEp1Application.class, args);
+    }
+
+}
